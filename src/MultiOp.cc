7a6753ace012e33c2e9495280354850b243d39f8
Alexander Merritt <merritt.alex@gatech.edu>
2015-01-18 23:56:53 -0500

    fix: compile error, use of std::make_pair
    removed use of function template

--- src/MultiOp.cc
+++ src/MultiOp.cc
@@ -142,8 +142,7 @@ MultiOp::dispatchRequest(MultiOpObject* request,
         (*queue)->reserve(PartRpc::MAX_OBJECTS_PER_RPC);
         (*queue)->push_back(request);
         sessionQueues.insert(
-            std::make_pair<Transport::SessionRef,
-                std::shared_ptr<SessionQueue>>(*session,
+            std::make_pair(*session,
                     std::shared_ptr<SessionQueue>(*queue)));
     } else {
         *queue = iter->second.get();
